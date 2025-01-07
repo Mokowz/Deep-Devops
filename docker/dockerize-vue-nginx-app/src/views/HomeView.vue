245e58c7d0a975fe69fd7ad979d56bf1da80c523
Ronny Mokaya <ronniemokaya30@gmail.com>
2025-01-07 15:56:42 +0300

    Dockerize a vue app without a web server

---
+++ docker/dockerize-vue-nginx-app/src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <p>Hey there</p>
+    <p>I have been tapped {{ count }}</p>
+    <button @click="increment">Tap Me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count += 1;
+    }
+  }
+}
+</script>
